6158c00fa469b15cbbd6242a352f6559cba664b4
Kyle Watt <kylewatt2000@hotmail.com>
2025-06-26 12:03:32 +0100

    Add guess and submit buttons.

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -8,7 +8,16 @@ def serve_quiz():
     """Serve the quiz frontend."""
     data = get_data(mode="ground")
     record = get_random_record(data)
-    st.markdown(f"Random record:\n{record}")
+
+    col_1, col_2 = st.columns([0.9, 0.1])
+    with col_1:
+        guess = st.text_input(label="guess",
+                              placeholder="Guess...",
+                              label_visibility="hidden")
+    with col_2:
+        submit = st.button(label="→")
+
+    col_1, col_2, col_3 = st.columns(3)
 
 
 if __name__ == "__main__":
